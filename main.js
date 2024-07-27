1e72afec02685db6c21d73243cc244dd61d8c54c
joãoA <joaovictor.dudu03@gmail.com>
2024-07-26 21:41:41 -0300

    implementando novo envento, 'CLICK'

--- main.js
+++ main.js
@@ -4,11 +4,18 @@ $(document).ready(function(){
         e.preventDefault()
 
         const inputTarefa = $("#tarefa").val()
-        const novaTarefa = $("<li></li>")
-
-        $(inputTarefa).appendTo(novaTarefa)
+        const novaTarefa = $("<li></li>").text(inputTarefa)
+        
         $(novaTarefa).appendTo("ul")
         $("#tarefa").val("")
-
     })
+
+    $("ul").on("click", "li", function(){
+        $(this).toggleClass("completed")
+    })
+
+    $("button[type='reset']").on("click", function(){
+        $("ul").empty();
+    });
+
 })
